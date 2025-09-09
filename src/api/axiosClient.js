74051460ae52bd5fa5a1043a33bdd1fbadc7013c
MahammadAnsar36 <145433315+MahammadAnsar36@users.noreply.github.com>
2025-09-09 20:50:20 +0530

    yes

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://thisiscinema-back.onrender.com", // ✅ LIVE backend URL
+  baseURL: "https://backend-an9o.onrender.com", // ✅ LIVE backend URL
   headers: {
     "Content-Type": "application/json"
   }
